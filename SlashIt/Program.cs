8f79d66eefb563d8a43614220f3ec2587d66c79a
Mark Kniffen <mark@kniffen.net>
2011-12-23 18:10:50 -0700

    added basic bounds checking.

--- SlashIt/Program.cs
+++ SlashIt/Program.cs
@@ -27,6 +27,7 @@ namespace SlashIt
             {
                 game.WriteConsole();
                 HandleInput();
+                game.CheckBounds();
             }
 
             Console.WriteLine("Game over!");
@@ -53,29 +54,307 @@ namespace SlashIt
         private static void HandleInput()
         {
             var keyInfo = Console.ReadKey(true);
-            Status.Message = "Key: " + keyInfo.Key;
+          //  Status.Message = "Key: " + keyInfo.Key;
 
-            if (keyInfo.Key == ConsoleKey.Q)
-            {
-                quit = true;
-            }
-            else if (keyInfo.Key == ConsoleKey.RightArrow)
-            {
-                game.character.MoveRight();
-            }
-            else if (keyInfo.Key == ConsoleKey.LeftArrow)
-            {
-                game.character.MoveLeft();
-            }
-            else if (keyInfo.Key == ConsoleKey.UpArrow)
-            {
-                game.character.MoveUp();
-            }
-            else if (keyInfo.Key == ConsoleKey.DownArrow)
-            {
-                game.character.MoveDown();
-            }
 
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.A:
+                    break;
+                case ConsoleKey.Add:
+                    break;
+                case ConsoleKey.Applications:
+                    break;
+                case ConsoleKey.Attention:
+                    break;
+                case ConsoleKey.B:
+                    break;
+                case ConsoleKey.Backspace:
+                    break;
+                case ConsoleKey.BrowserBack:
+                    break;
+                case ConsoleKey.BrowserFavorites:
+                    break;
+                case ConsoleKey.BrowserForward:
+                    break;
+                case ConsoleKey.BrowserHome:
+                    break;
+                case ConsoleKey.BrowserRefresh:
+                    break;
+                case ConsoleKey.BrowserSearch:
+                    break;
+                case ConsoleKey.BrowserStop:
+                    break;
+                case ConsoleKey.C:
+                    break;
+                case ConsoleKey.Clear:
+                    break;
+                case ConsoleKey.CrSel:
+                    break;
+                case ConsoleKey.D:
+                    break;
+                case ConsoleKey.D0:
+                    break;
+                case ConsoleKey.D1:
+                    break;
+                case ConsoleKey.D2:
+                    break;
+                case ConsoleKey.D3:
+                    break;
+                case ConsoleKey.D4:
+                    break;
+                case ConsoleKey.D5:
+                    break;
+                case ConsoleKey.D6:
+                    break;
+                case ConsoleKey.D7:
+                    break;
+                case ConsoleKey.D8:
+                    break;
+                case ConsoleKey.D9:
+                    break;
+                case ConsoleKey.Decimal:
+                    break;
+                case ConsoleKey.Delete:
+                    break;
+                case ConsoleKey.Divide:
+                    break;
+                case ConsoleKey.DownArrow:
+                    game.character.Move(ConsoleKey.DownArrow);
+                    break;
+                case ConsoleKey.E:
+                    break;
+                case ConsoleKey.End:
+                    break;
+                case ConsoleKey.Enter:
+                    break;
+                case ConsoleKey.EraseEndOfFile:
+                    break;
+                case ConsoleKey.Escape:
+                    break;
+                case ConsoleKey.ExSel:
+                    break;
+                case ConsoleKey.Execute:
+                    break;
+                case ConsoleKey.F:
+                    break;
+                case ConsoleKey.F1:
+                    break;
+                case ConsoleKey.F10:
+                    break;
+                case ConsoleKey.F11:
+                    break;
+                case ConsoleKey.F12:
+                    break;
+                case ConsoleKey.F13:
+                    break;
+                case ConsoleKey.F14:
+                    break;
+                case ConsoleKey.F15:
+                    break;
+                case ConsoleKey.F16:
+                    break;
+                case ConsoleKey.F17:
+                    break;
+                case ConsoleKey.F18:
+                    break;
+                case ConsoleKey.F19:
+                    break;
+                case ConsoleKey.F2:
+                    break;
+                case ConsoleKey.F20:
+                    break;
+                case ConsoleKey.F21:
+                    break;
+                case ConsoleKey.F22:
+                    break;
+                case ConsoleKey.F23:
+                    break;
+                case ConsoleKey.F24:
+                    break;
+                case ConsoleKey.F3:
+                    break;
+                case ConsoleKey.F4:
+                    break;
+                case ConsoleKey.F5:
+                    break;
+                case ConsoleKey.F6:
+                    break;
+                case ConsoleKey.F7:
+                    break;
+                case ConsoleKey.F8:
+                    break;
+                case ConsoleKey.F9:
+                    break;
+                case ConsoleKey.G:
+                    break;
+                case ConsoleKey.H:
+                    break;
+                case ConsoleKey.Help:
+                    break;
+                case ConsoleKey.Home:
+                    break;
+                case ConsoleKey.I:
+                    break;
+                case ConsoleKey.Insert:
+                    break;
+                case ConsoleKey.J:
+                    break;
+                case ConsoleKey.K:
+                    break;
+                case ConsoleKey.L:
+                    break;
+                case ConsoleKey.LaunchApp1:
+                    break;
+                case ConsoleKey.LaunchApp2:
+                    break;
+                case ConsoleKey.LaunchMail:
+                    break;
+                case ConsoleKey.LaunchMediaSelect:
+                    break;
+                case ConsoleKey.LeftArrow:
+                    game.character.Move(ConsoleKey.LeftArrow);
+                    break;
+                case ConsoleKey.LeftWindows:
+                    break;
+                case ConsoleKey.M:
+                    break;
+                case ConsoleKey.MediaNext:
+                    break;
+                case ConsoleKey.MediaPlay:
+                    break;
+                case ConsoleKey.MediaPrevious:
+                    break;
+                case ConsoleKey.MediaStop:
+                    break;
+                case ConsoleKey.Multiply:
+                    break;
+                case ConsoleKey.N:
+                    break;
+                case ConsoleKey.NoName:
+                    break;
+                case ConsoleKey.NumPad0:
+                    break;
+                case ConsoleKey.NumPad1:
+                    break;
+                case ConsoleKey.NumPad2:
+                    break;
+                case ConsoleKey.NumPad3:
+                    break;
+                case ConsoleKey.NumPad4:
+                    break;
+                case ConsoleKey.NumPad5:
+                    break;
+                case ConsoleKey.NumPad6:
+                    break;
+                case ConsoleKey.NumPad7:
+                    break;
+                case ConsoleKey.NumPad8:
+                    break;
+                case ConsoleKey.NumPad9:
+                    break;
+                case ConsoleKey.O:
+                    break;
+                case ConsoleKey.Oem1:
+                    break;
+                case ConsoleKey.Oem102:
+                    break;
+                case ConsoleKey.Oem2:
+                    break;
+                case ConsoleKey.Oem3:
+                    break;
+                case ConsoleKey.Oem4:
+                    break;
+                case ConsoleKey.Oem5:
+                    break;
+                case ConsoleKey.Oem6:
+                    break;
+                case ConsoleKey.Oem7:
+                    break;
+                case ConsoleKey.Oem8:
+                    break;
+                case ConsoleKey.OemClear:
+                    break;
+                case ConsoleKey.OemComma:
+                    break;
+                case ConsoleKey.OemMinus:
+                    break;
+                case ConsoleKey.OemPeriod:
+                    break;
+                case ConsoleKey.OemPlus:
+                    break;
+                case ConsoleKey.P:
+                    break;
+                case ConsoleKey.Pa1:
+                    break;
+                case ConsoleKey.Packet:
+                    break;
+                case ConsoleKey.PageDown:
+                    break;
+                case ConsoleKey.PageUp:
+                    break;
+                case ConsoleKey.Pause:
+                    break;
+                case ConsoleKey.Play:
+                    break;
+                case ConsoleKey.Print:
+                    break;
+                case ConsoleKey.PrintScreen:
+                    break;
+                case ConsoleKey.Process:
+                    break;
+                case ConsoleKey.Q: 
+                    quit = true;
+                    break;
+                case ConsoleKey.R:
+                    break;
+                case ConsoleKey.RightArrow:
+                    game.character.Move(ConsoleKey.RightArrow);
+                    break;
+                case ConsoleKey.RightWindows:
+                    break;
+                case ConsoleKey.S:
+                    break;
+                case ConsoleKey.Select:
+                    break;
+                case ConsoleKey.Separator:
+                    break;
+                case ConsoleKey.Sleep:
+                    break;
+                case ConsoleKey.Spacebar:
+                    break;
+                case ConsoleKey.Subtract:
+                    break;
+                case ConsoleKey.T:
+                    break;
+                case ConsoleKey.Tab:
+                    break;
+                case ConsoleKey.U:
+                    break;
+                case ConsoleKey.UpArrow:
+                    game.character.Move(ConsoleKey.UpArrow);
+                    break;
+                case ConsoleKey.V:
+                    break;
+                case ConsoleKey.VolumeDown:
+                    break;
+                case ConsoleKey.VolumeMute:
+                    break;
+                case ConsoleKey.VolumeUp:
+                    break;
+                case ConsoleKey.W:
+                    break;
+                case ConsoleKey.X:
+                    break;
+                case ConsoleKey.Y:
+                    break;
+                case ConsoleKey.Z:
+                    break;
+                case ConsoleKey.Zoom:
+                    break;
+                default:
+                    break;
+            };
         }
     }
 
@@ -85,6 +364,8 @@ namespace SlashIt
         public Character character;
 
         private Map map;
+        public const short MapStartLeft = 20;
+        public const short MapStartTop = 3;
 
         public void InitConsole()
         {
@@ -124,6 +405,8 @@ namespace SlashIt
             Console.Write("@");
             character.SetPositionBeforeMove();
 
+            Status.Message = "Map Left: " + character.LeftMapPosition + " :MapTop: " + character.TopMapPosition;
+
             Status.WriteToStatusLine();
 
             //TODO add console bounds check
@@ -132,8 +415,8 @@ namespace SlashIt
 
         internal void GenerateMap()
         {
-            int mapStartLeft = 20;
-            int mapStartTop = 3;
+            int mapStartLeft = Game.MapStartLeft;
+            int mapStartTop = Game.MapStartTop;
 
             Console.SetCursorPosition(mapStartLeft, mapStartTop);
 
@@ -152,7 +435,6 @@ namespace SlashIt
                         default:
                             break;
                     }
-
                 }
 
                 Console.SetCursorPosition(mapStartLeft, ++mapStartTop);
@@ -162,12 +444,21 @@ namespace SlashIt
             map.MapOutdated = false;
 
         }
+
+        internal void CheckBounds()
+        {
+            //TODO -- Improve this
+            if (map.map[character.TopMapPosition, character.LeftMapPosition] != 0)
+            {
+                character.DisallowMove();
+            }
+        }
     }
 
 
     public class Status
     {
-        //TODO: maybe change this to be inited on porg start so that it can very more easiyl 
+        //TODO: maybe change this to be inited on porg start so that it can very more easily 
         private static string clearLine = "                                                                               ";
 
         private const int MessagePositionDefaultLeft = 0;
@@ -214,6 +505,15 @@ namespace SlashIt
 
         public int LeftBeforeMove { get; set; }
         public int TopBeforeMove { get; set; }
+        
+        public int LeftMapPosition { get { return this.Left - Game.MapStartLeft; } }
+        public int TopMapPosition { get { return this.Top - Game.MapStartTop; } }
+
+        public void DisallowMove()
+        {
+            this.Left = this.LeftBeforeMove;
+            this.Top = this.TopBeforeMove;
+        }
 
         public void SetPosition(int left, int top)
         {
@@ -223,34 +523,54 @@ namespace SlashIt
             this.SetPositionBeforeMove();
         }
 
-        public void MoveRight()
-        {
-            //this.LeftBeforeMove = this.Left;
-            this.Left++;
-        }
+        //public void MoveRight()
+        //{
+            
+        //}
 
-        public void MoveLeft()
-        {
-            //this.LeftBeforeMove = this.Left;
-            this.Left--;
-        }
+        //public void MoveLeft()
+        //{
+            
+        //}
 
-        public void MoveUp()
-        {
-            //this.TopBeforeMove = this.Top;
-            this.Top--;
-        }
+        //public void MoveUp()
+        //{
+        //    this.Top--;
+        //}
 
-        public void MoveDown()
-        {
-            //this.TopBeforeMove = this.Top;
-            this.Top++;
-        }
+        //public void MoveDown()
+        //{
+        //}
 
         public void SetPositionBeforeMove()
         {
             this.LeftBeforeMove = this.Left;
             this.TopBeforeMove = this.Top;
+        }
+
+        public void Move(ConsoleKey consoleKey)
+        {
+            switch (consoleKey)
+            {
+                case ConsoleKey.UpArrow:
+                    this.Top--;
+                    break;
+
+                case ConsoleKey.DownArrow:
+                    this.Top++;
+                    break;
+
+                case ConsoleKey.LeftArrow:
+                    this.Left--;        
+                    break;
+
+                case ConsoleKey.RightArrow:
+                    this.Left++;
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 
@@ -277,21 +597,6 @@ namespace SlashIt
               { 1,1,0,0,0,0,0,0,0,1 }, 
               { 1,1,1,1,1,1,1,1,1,1 }, 
             };
-
-
-
-
-
-
-        //Console.Write("###########################"); Console.SetCursorPosition(mapStartLeft, mapStartTop + 1);
-        //Console.Write("#               +       ###"); Console.SetCursorPosition(mapStartLeft, mapStartTop + 2);
-        //Console.Write("############### ###########"); Console.SetCursorPosition(mapStartLeft, mapStartTop + 3);
-        //Console.Write("############### #####    ##"); Console.SetCursorPosition(mapStartLeft, mapStartTop + 4);
-        //Console.Write("########        ##### #####"); Console.SetCursorPosition(mapStartLeft, mapStartTop + 5);
-        //Console.Write("######## ############ #####"); Console.SetCursorPosition(mapStartLeft, mapStartTop + 6);
-        //Console.Write("########              #####"); Console.SetCursorPosition(mapStartLeft, mapStartTop + 7);
-        //Console.Write("###########################");
-            
 
     }
 }
