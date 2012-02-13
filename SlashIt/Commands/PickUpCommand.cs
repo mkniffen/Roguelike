bfee1e097d1d5b3bf0f8cf276e0ad5f3a5c92006
Mark Kniffen <mark@kniffen.net>
2012-02-12 17:48:58 -0700

    A new pick up command.

---
+++ SlashIt/Commands/PickUpCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SlashIt
+{
+    public class PickUpCommand : ICommand
+    {
+        Map map;
+
+
+        //TODO !!!!!!!  WORKING HERE  !!!!!!  Implement inventory
+
+
+        public PickUpCommand(Map map)
+        {
+            this.map = map;
+        }
+
+        public void execute(LocalKeyInfo keyInfo)
+        {
+            Tile tile = this.map.GetPlayerTile();
+            if (tile.HasItem())
+            {
+                Status.Info = "You take " + tile.Item.Name + ".";
+            }
+            else 
+            {
+                Status.Info = "This is nothing to pick up here.";
+            }
+        }
+    }
+}
---
+++ SlashIt/Item.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace SlashIt
+{
+    
+
+    public class Item
+    {
+        public static List<Item> availableItems = new List<Item>
+        {
+            { new Item { Name = "Bob's wallet", DisplayCharacter="~", TypeId=100000, Description = "You found Bob's wallet.  He's gonna be REAL mad if he finds out you have it"}},
+        };
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string DisplayCharacter { get; set; }
+        public int TypeId { get;set; }
+
+        public static Item GetItemById(int itemId)
+        {
+ 	        return availableItems.Where(i => i.TypeId == itemId).Single<Item>();
+        }
+
+        public void Load(XElement item)
+        {
+ 	        this.TypeId = Int32.Parse(item.Element("TypeId").Value);
+        }
+
+        public XElement Save()
+        {
+ 	        return new XElement("Item",
+                new XElement("TypeId", this.TypeId)
+                );
+        }
+    }
+}
